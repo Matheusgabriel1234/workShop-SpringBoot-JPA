aa758ec8636bc8fb8ec12463a18352164aa341c4
Matheusgabriel1234 <magaje2005@gmail.com>
2024-04-10 13:08:55 -0300

    Commit inicial

---
+++ src/test/java/com/matheusGabriel1234/Project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusGabriel1234.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
