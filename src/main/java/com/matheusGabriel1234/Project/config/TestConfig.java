a8a801792c5bf683ad80463b07080cde7720b99a
Matheusgabriel1234 <magaje2005@gmail.com>
2024-04-10 14:14:54 -0300

    Injeção de dependencia.database seeding

--- src/main/java/com/matheusGabriel1234/Project/config/TestConfig.java
+++ src/main/java/com/matheusGabriel1234/Project/config/TestConfig.java
@@ -1,5 +1,29 @@
 package com.matheusGabriel1234.Project.config;
 
-public class TestConfig {
+import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.matheusGabriel1234.Project.entities.Users;
+import com.matheusGabriel1234.Project.repository.UserRepository;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner{
+
+	@Autowired
+	private UserRepository repo;
+
+	@Override
+	public void run(String... args) throws Exception {
+ Users pedro = new Users(null,"Pedro Gabriel","pedro@111111.com","(11)11111-1111","pedro123");
+ Users eduarda = new Users(null,"Eduarda Cecato","eduarda@111111.com","(11)11111-1111","eduarda123"); 
+ 
+ repo.saveAll(Arrays.asList(pedro,eduarda));
+		
+	}
+	
 }
--- src/main/java/com/matheusGabriel1234/Project/repository/UserRepository.java
+++ src/main/java/com/matheusGabriel1234/Project/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.matheusGabriel1234.Project.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.matheusGabriel1234.Project.entities.Users;
+
+public interface UserRepository extends JpaRepository<Users, Long> {
 
 }
