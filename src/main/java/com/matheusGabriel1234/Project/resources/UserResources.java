3c9db1c624ec7a71b227fb218a49e1b3a0936032
Matheusgabriel1234 <magaje2005@gmail.com>
2024-04-11 13:42:20 -0300

    Metódo findByid e Service entity

--- src/main/java/com/matheusGabriel1234/Project/resources/UserResources.java
+++ src/main/java/com/matheusGabriel1234/Project/resources/UserResources.java
@@ -1,19 +1,37 @@
 package com.matheusGabriel1234.Project.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.matheusGabriel1234.Project.entities.Users;
+import com.matheusGabriel1234.Project.services.UserServices;
 
 @RestController
 @RequestMapping(value="/users")
 public class UserResources {
 
+	@Autowired
+	private UserServices serv;
 	@GetMapping
-	public ResponseEntity<Users> findAll(){
-		Users u = new Users(1L, "Matheus Gabriel", "matheus@11111", "11-999999999","roma123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Users>> findAll(){
+		
+		List<Users> users = serv.findAll();
+		
+		
+		return ResponseEntity.ok().body(users);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Users> getId(@PathVariable Long id) {
+		Users obj = serv.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/matheusGabriel1234/Project/services/UserServices.java
@@ -0,0 +1,32 @@
+package com.matheusGabriel1234.Project.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusGabriel1234.Project.entities.Users;
+import com.matheusGabriel1234.Project.repository.UserRepository;
+
+@Service
+
+public class UserServices {
+
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<Users> findAll(){
+		return repo.findAll();
+		
+	}
+	
+	public Users findById(Long id) {
+		Optional<Users> obj = repo.findById(id);
+		return obj.get();
+		 
+		
+	}
+	
+}
